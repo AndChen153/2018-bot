8b37b31648fc696829f81bef7286996bfac2b43d
Will Eggleston <work.willeggleston@gmail.com>
2018-03-12 15:24:16 -0500

    python3

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,2 +1,2 @@
-python robot/generate_paths.py robot/paths/ --generate-csvs
-python robot/robot.py deploy --skip-tests
+python3 robot/generate_paths.py robot/paths/ --generate-csvs
+python3 robot/robot.py deploy --skip-tests
